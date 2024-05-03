4310d208f902f8d8e7c1c8cf53a42819b5640e9e
petterSantos <newpetter@gmail.com>
2024-05-03 17:16:35 -0500

    ekaboracion modelo incidencia

--- main/models.py
+++ main/models.py
@@ -27,3 +27,40 @@ class Configure(models.Model):
     value = models.CharField(max_length=500)
     def __str__(self):
         return self.key
+
+class TipoEquipo(models.Model):
+    tipo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=500)
+    activo = models.BooleanField(default=True)
+    def __str__(self):
+        return self.tipo
+
+class Equipo(models.Model):
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    codPatrimonial = models.CharField(max_length=20)
+    descripcionEquipo =  models.TextField(blank=True)
+    descripcionCompra =  models.TextField(blank=True)
+    estadoPatrimonio = models.CharField(max_length=1) # M R B
+    responsablePatri = models.CharField(max_length=100)
+    oficinaPatri = models.CharField(max_length=100)
+    fechaUpdate = models.DateTimeField(null=True)
+    tipoEquipo = models.ForeignKey(TipoEquipo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.tipo
+
+class Incidencia(models.Model):
+    solicita = models.CharField(max_length=100)
+    oficina = models.ForeignKey(Oficina,on_delete=models.CASCADE)
+    solicitante = models.CharField(max_length=100)
+    codSolicitante = models.IntegerField(null=False)
+    cargoUsuario = models.CharField(max_length=100)
+    solicitaConDoc =models.BooleanField(default=False)
+    nroDoc = models.CharField(max_length=400, blank = True, null=True)
+    fechaSolicita = models.DateTimeField(auto_now_add=True)
+    dejaEquipo =models.BooleanField(default=False)
+    userTic = models.ForeignKey(User,on_delete=models.CASCADE)
+    area = models.ForeignKey(Area,on_delete=models.CASCADE)
+    fechaTermina = models.DateTimeField(null=True)
+    def __str__(self):
+        return self.key
